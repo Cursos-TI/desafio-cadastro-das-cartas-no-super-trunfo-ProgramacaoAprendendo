a8a2889b84226d612bc5ef5a5df1f39ed8a8568a
ProgramacaoAprendendo <joaodaruy1234@gmail.com>
2025-02-22 09:10:55 +0000

    abc

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,5 +10,20 @@ int main(){
     float pib;
     int turisticos;
 
+    printf("Estado cidade 1: \n");
+    scanf("%c", &estado);
+    printf("Codigo da cidade 1: \n");
+    scanf("%s", &codigo);
+    printf("Nome da cidade 1: \n");
+    scanf("%s", &nome);
+    printf("População da cidade 1: \n");
+    scanf("%d", &populacao);
+    printf("Area da cidade 1: \n");
+    scanf("%f", &area);
+    printf("PIB da cidade 1: \n");
+    scanf("%f", &pib);
+    printf("Número de pontos turísticos da cidade 1: \n");
+    scanf("%d", &turisticos);
+
 
 }
